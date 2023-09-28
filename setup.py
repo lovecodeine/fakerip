c3fad66be2fefd987db6d60f2ed5ad5b85b76ae9
lovecodeine <igot2pints@gmail.com>
2023-09-28 21:16:36 +0200

    version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fakerip',
-    version='0.4',
+    version='1.0',
     packages=find_packages(),
     install_requires=['requests-html'],
     author='purplevoid',
